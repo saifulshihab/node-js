9103a8e620fec99f798375ec663fb5c2af9d2266
SaifulShihab <shihabmd1970@gmail.com>
2020-09-10 09:08:55 +0600

    apply promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,51 +1,52 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const db_operation = require('./operations');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null);
+MongoClient.connect(url)
+  .then((client) => {
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
 
-  console.log('Connected to MongoDB server!!');
+    dboper
+      .insertDocument(db, { name: 'Vadonut', description: 'Test' }, 'dishes')
+      .then((result) => {
+        console.log('Insert Document:\n', result.ops);
 
-  const db = client.db(dbname);
-
-  db_operation.insertDocument(
-    db,
-    { name: 'Vadonut', description: 'Test' },
-    'dishes',
-    (result) => {
-      console.log('Insert Document:\n', result.ops);
-
-      db_operation.findDocuments(db, 'dishes', (docs) => {
+        return dboper.findDocuments(db, 'dishes');
+      })
+      .then((docs) => {
         console.log('Found Documents:\n', docs);
 
-        db_operation.updateDocument(
+        return dboper.updateDocument(
           db,
           { name: 'Vadonut' },
           { description: 'Updated Test' },
-          'dishes',
-          (result) => {
-            console.log('Updated Document:\n', result.result);
-
-            db_operation.findDocuments(db, 'dishes', (docs) => {
-              console.log('Found Updated Documents:\n', docs);
-
-              db.dropCollection('dishes', (result) => {
-                console.log('Dropped Collection: ', result);
-
-                client.close();
-              });
-            });
-          }
+          'dishes'
         );
-      });
-    }
-  );
+      })
+      .then((result) => {
+        console.log('Updated Document:\n', result.result);
 
-  /* const collection = db.collection('dishes');
+        return dboper.findDocuments(db, 'dishes');
+      })
+      .then((docs) => {
+        console.log('Found Updated Documents:\n', docs);
+
+        return db.dropCollection('dishes');
+      })
+      .then((result) => {
+        console.log('Dropped Collection: ', result);
+
+        return client.close();
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
+
+/* const collection = db.collection('dishes');
 
   collection.insertOne(
     { name: 'Hot Pizza', description: 'Very hot' },
@@ -67,4 +68,3 @@ MongoClient.connect(url, (err, client) => {
       });
     }
   ); */
-});
