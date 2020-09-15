8c4454da44056deeb36663bf79f33c4ddfea6138
SaifulShihab <shihabmd1970@gmail.com>
2020-09-15 11:39:58 +0600

    using cookies intro

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -30,31 +30,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-5675675-32457834-23'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    let err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-  const auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-    .toString()
-    .split(':');
-  const user = auth[0];
-  const pass = auth[1];
-  if (user == 'admin' && pass == 'admin') {
-    next();
+  if (!req.signedCookies.user) {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
+      .toString()
+      .split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user == 'admin' && pass == 'admin') {
+      res.cookie('user', 'admin', { signed: true });
+      next(); //authorized
+    } else {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
   } else {
-    let err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
+    if (req.signedCookies.user === 'admin') {
+      next();
+    } else {
+      let err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
   }
 }
 app.use(auth);
